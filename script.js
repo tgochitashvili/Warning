b46020ca482d27da7d0787e7a46722170b95612d
tgochitashvili <tazo.gochitashvili@gmail.com>
2020-08-09 06:03:25 +0400

    Updated style, added call animations

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-
-
 /*jshint esversion: 6 */
 let defaultwarningmessage = "This sometimes happens when microphone access has been disabled.";
-let warningmessage = defaultwarningmessage;
 $(document).ready(function(){
     var warningcontent = $("#warningcontent");
     var acceptbtn = $("#accept");
     var callwindow = $("#callwindow");
-
-
-    warningcontent.html(warningmessage);
     acceptbtn.hover(
         function() { 
             if (acceptbtn.hasClass("inactivecallbutton"))
@@ -26,17 +20,7 @@ $(document).ready(function(){
       })
       .catch(function(err) {
         acceptbtn.addClass("inactivecallbutton");
-        if (err.message === "Requested device not found"){
-            warningmessage = "Your device does not seem to have an active microphone";
-            warningcontent.html(warningmessage);
-        }
-        else if (err.message === "Permission denied"){
-            warningmessage = "Microphone access has been disabled for this website";
-            warningcontent.html(warningmessage);
-        }
-        else{
-            warningcontent.html(defaultwarningmessage);
-        }
+        warningcontent.html(defaultwarningmessage);
       });
     $(".togglebtn").click(function(){
         callwindow.fadeTo("fast",1.0);
